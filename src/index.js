ebcad849b4cdc0156bcf0fd7b3b8a75805cf1251
Jyoti <beherajyotiranjan2017@gmail.com>
2024-12-09 23:19:47 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -5,4 +5,6 @@ app.get('/', (req, res) => {
   res.send('Welcome to the Node.js CI/CD Pipeline Example!');
 });
 
+app.listen(3000, () => console.log('Server running on port 3000'));
+
 module.exports = app;
